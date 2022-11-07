9373266ad49dddf1e35497779abc710e456845ab
Raghav Mittal <raghavmittal648@gmail.com>
2022-11-08 00:04:11 +0530

    add c

--- sum.java
+++ sum.java
@@ -3,7 +3,8 @@ public class sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
-        System.out.println(a + b);
+        int c = 7;
+        System.out.println(a + b + c);
 
     }
 }
